59058bd5da5ef1ddc884d51737094f5f68e4f4aa
Ayush Kumar <ayushrocks456@gmail.com>
2022-12-12 00:18:10 +0530

    integrated opengl window

--- Physics2d/main.cpp
+++ Physics2d/main.cpp
@@ -1,7 +1,40 @@
 #include"Chakra\core.h"
+#include<glad\glad.h>
+#include<glfw3.h>
 using namespace Chakra;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Physics Engine", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "FAILED TO CREATE WINDOW" << "\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "FAILED TO INITIALIZE GLAD";
+		return -1;
+
+	}
+	glViewport(0, 0, 800, 600);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	Chakra::Vector3 test(0, 1, 1);
 	test.Display();
 	std::cout << "\n"<<test.magnitude();
---
+++ Physics2d/particle.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"Chakra/core.h"
+namespace Chakra {
+	class Particle {
+	protected:
+		Vector3 particle;
+		Vector3 position;
+		Vector3 acceleration;
+	};
+}
