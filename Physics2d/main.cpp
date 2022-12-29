ace254ca72a154f092c0bccf2199155a75c5bf03
Ayush Kumar <ayushrocks456@gmail.com>
2022-12-29 20:33:35 +0530

    first triangle

--- Physics2d/main.cpp
+++ Physics2d/main.cpp
@@ -15,36 +15,37 @@ const char* vertexShaderSource = "#version 330 core\n"
 "}\0";
 
 const char* fragmentShaderSource =
+"#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n";
+"   FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
+"}\n\0";
 int main()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	float triangleSideLength = 0.2; // this is normalised
-	Vector3 a(0.5, 0.5, 0);
-	Vector3 b(0.5 + triangleSideLength, 0.5, 0);
-	Vector3 c(0.5 + triangleSideLength / 2, 0.5 + triangleSideLength / 2, 0);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Physics Engine", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "FAILED TO CREATE WINDOW" << "\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
-	float vertices[] = {
-		a.x, a.y, a.z,
-		b.x, b.y, b.z,
-		c.x, c.y, c.z
-	};
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "FAILED TO INITIALIZE GLAD";
+		return -1;
 
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	}
+	glViewport(0, 0, 800, 600);
 
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 	int success;
@@ -57,8 +58,7 @@ int main()
 	}
 
 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 	int fragSuccess;
@@ -83,51 +83,55 @@ int main()
 	if (!programSuccess)
 	{
 		glGetProgramInfoLog(shaderProgram, 512, NULL, programSuccessLog);
-		std::cout << "ERROR::PROGRAM::COMPILATION FAILED\n" << fragInfoLog << std::endl;
+		std::cout << "ERROR::PROGRAM::COMPILATION FAILED\n" << programSuccessLog << std::endl;
 	}
+	float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+	};
 
-	glUseProgram(shaderProgram);
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	unsigned int VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Physics Engine", NULL, NULL);
-	if (!window)
-	{
-		std::cout << "FAILED TO CREATE WINDOW" << "\n";
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+	glBindVertexArray(0);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "FAILED TO INITIALIZE GLAD";
-		return -1;
-
-	}
-	glViewport(0, 0, 800, 600);
+	
 	while (!glfwWindowShouldClose(window))
 	{
 		
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 		
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 
 	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	glfwTerminate();
 	Chakra::Vector3 test(0, 1, 1);
 	test.Display();
